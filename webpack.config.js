14dec6ca32557ced85bb07dc8a177fa7acc886fc
Ineigo <ineigokira@gmail.com>
2019-04-26 11:29:49 +0300

    Обновлен webpack

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
     },
     devtool: 'source-map',
     module: {
-        loaders: [
+        rules: [
             {
                 exclude: /(node_modules)/,
                 loader: 'babel-loader',
